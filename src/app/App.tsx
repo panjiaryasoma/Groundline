a7c3db01074e17ca5c317ee82051ce2aaf0cb34f
Panji Arya Soma <214255890+panjiaryasoma@users.noreply.github.com>
2026-09-03 21:27:43 +0700

    P12.3: tighten section proportions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,7 @@ import "../styles/app.css";
 import "../styles/p11.css";
 import "../styles/p12.css";
 import "../styles/p12-2.css";
+import "../styles/p12-3.css";
 
 installP111RepairLifecycle();
 installP112CustomSemanticGate();
